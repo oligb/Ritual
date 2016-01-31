d9433222958b19fd5b9041794a6d2bdf2d1fe528
Matthew Conto <mbsconto@gmail.com>
2016-01-31 12:58:17 -0500

    Very latest I have

--- Assets/Scripts/Segment.cs
+++ Assets/Scripts/Segment.cs
@@ -1,7 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Segment : MonoBehaviour {
+    
+    public float baseLength;
+    public int numberOfPiecesToChoose = 2;
+    public List<GameObject> leftPieces;
+    public List<GameObject> rightPieces; 
+    public List<GameObject> roofPieces;
 
 	// Use this for initialization
 	void Start () {
@@ -14,6 +21,20 @@ public class Segment : MonoBehaviour {
 	}
     
     public void AssembleSegment( float distance ) {
-        
+        List<int> pieces = new List<int>();
+        for( int i = 0; i < numberOfPiecesToChoose; i++ ) {
+            int index = Random.Range( 0, leftPieces.Count + rightPieces.Count - 1 );
+            while( pieces.Contains(index) ) {
+                index = Random.Range( 0, leftPieces.Count + rightPieces.Count - 1 );
+            }
+            pieces.Add( index );
+        }
+        foreach( int index in pieces ) {
+            if( index < leftPieces.Count ) {
+                leftPieces[index].SetActive(true);
+            } else {
+                rightPieces[ index - leftPieces.Count ].SetActive(true);
+            }
+        }
     }
 }
